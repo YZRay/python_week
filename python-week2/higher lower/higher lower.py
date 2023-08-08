878173546c65da5b19688a872a0aada8ec896456
姚澤睿 <130679012+YZRay@users.noreply.github.com>
2023-08-08 10:52:35 +0800

    Add files via upload

---
+++ python-week2/higher lower/higher lower.py
@@ -0,0 +1,53 @@
+from art import logo, vs
+from data import data
+import random
+import os
+def clear(): return os.system('cls')
+
+
+def format_data(account):
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+while game_should_continue:
+
+    account_a = account_b
+    account_b = random.choice(data)
+
+    while account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}.")
+
+    guess = input("Who has more followers? type'A' or 'B': ").lower()
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        game_should_continue = False
+        print(f"You're wrong. Final score: {score}")
